416822633f0d438803f44eb1005d1e888f8da29c
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2019-12-30 21:32:36 +0100

    Use Serilog

--- src/SthlmTechEvents.App/FetchEvents.cs
+++ src/SthlmTechEvents.App/FetchEvents.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -10,10 +11,12 @@ namespace SthlmTechEvents.App
     public class FetchEvents
     {
         private readonly IEventPublisherService _eventPublisherService;
+        private readonly ILogger<FetchEvents> _logger;
 
-        public FetchEvents(IEventPublisherService eventPublisherService)
+        public FetchEvents(IEventPublisherService eventPublisherService, ILogger<FetchEvents> logger)
         {
             _eventPublisherService = eventPublisherService;
+            _logger = logger;
         }
         
         [FunctionName("FetchEvents")]
@@ -21,12 +24,14 @@ namespace SthlmTechEvents.App
 #if DEBUG
                 , RunOnStartup = true // TODO: Figure out why it won't run in debug mode         
 #endif
-                )] 
+                )]     
             TimerInfo myTimer, 
             ILogger log)
         {
+            var stopWatch = Stopwatch.StartNew();
             await _eventPublisherService.PublishEvents();
-            log.LogInformation($"Fetch events function executed at: {DateTime.UtcNow}");
+            stopWatch.Stop();
+            _logger.LogInformation($"Fetch events function executed at: {DateTime.UtcNow:yyyy-MM-dd HH:mmss} in {stopWatch.ElapsedMilliseconds} ms.");
         }
     }
 }
--- src/SthlmTechEvents.App/Startup.cs
+++ src/SthlmTechEvents.App/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using SthlmTechEvents.BusinessLogic;
 
 [assembly: FunctionsStartup(typeof(SthlmTechEvents.App.Startup))]
@@ -10,6 +12,16 @@ namespace SthlmTechEvents.App
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            builder.Services.AddLogging(
+                lb => lb
+                    .AddSerilog(
+                        new LoggerConfiguration()
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console()
+                            .WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces)
+                            .CreateLogger(),
+                        true));
+            
             builder.Services.AddTransient<IEventPublisherService, EventPublisherService>();
             builder.Services.AddTransient<IEventFetcher, MeetupEventFetcher>();
             builder.Services.AddTransient<IEventFetcher, EventbriteEventFetcher>();
